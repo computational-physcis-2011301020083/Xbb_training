ddd9ac10f8817fead7d6dda7db367a83088aa828
Wei DIng <2432001232@qq.com>
2019-06-02 21:28:53 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,27 @@
+#How to run
+
 :<<EOF
+path="Dijets sample path"
+python CalculatedDijetsWeights.py --path $path
+#EOF
+
+path="Dijets sample path"
+python labelDijetsDatasets.py --path $path
+path="Top sample path"
+python labelTopDatasets.py --path $path
+path="Hbb sample path"
+python labelHbbDatasets.py --path $path
+#EOF
+
+for i in TopSamplePath HbbSamplePtah
+do 
+  python MergeDatasets.py --path $path
+done
+
+path="Dijets sample path"
+python MergeDijetsDatasets.py --path $path
+#EOF
+
 for i in $(ls MergedData)
 do
   path="MergedData/"$i
@@ -21,6 +44,4 @@ EOF
 
 path="SplitData"
 python train.py --path $path
-
-
 
