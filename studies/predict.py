f7806924a124fafae763d699a4e1fa3fc4bb329e
Wei DIng <2432001232@qq.com>
2019-06-11 10:11:27 -0700

    Add files via upload

--- studies/predict.py
+++ studies/predict.py
@@ -6,25 +6,6 @@ parser.add_argument("--path", dest='path',  default="", help="path")
 parser.add_argument("--model", dest='model',  default="", help="model")
 args = parser.parse_args()
 
-
-#load mass and pt
-def load_mass(data,style):
-  load_f=h5py.File(data)
-  load_fat=load_f.get("fat_jet/"+style)
-  print load_fat[:,2].shape
-  return load_fat[:,2]
- 
-#dijet: (865594,)
-#signal: (212821,)
-#top: (529776,)
-
-files=sorted(glob.glob("SplitData/*.h5"))
-bg_m=load_mass(files[0],"test")
-signal_m=load_mass(files[1],"test")
-top_m=load_mass(files[2],"test")
-mass=np.hstack((bg_m,signal_m,top_m))
-
-
 model_file=args.model
 test_data=args.path
 model = keras.models.load_model(model_file)
@@ -32,18 +13,24 @@ load_file=h5py.File(test_data)
 data=load_file.get("test/data")
 y=load_file.get("test/y")
 w=load_file.get("test/w")
+'''
+test_index=np.random.choice(w.shape[0],200000,replace=False)
+data=np.take(data,test_index,axis=0)
+y=np.take(y,test_index,axis=0)
+w=np.take(w,test_index,axis=0)
+'''
+
 predictions = model.predict(data)
-y=y[:,1]
+#y=y[:,1]
 score=predictions[:,1]
+save_f = h5py.File("PrepareData/prediction.h5", 'w')
 
-save_f = h5py.File("predict1.h5", 'w')
-y=np.reshape(y,(y.shape[0],1))
+#y=np.reshape(y,(y.shape[0],1))
 score=np.reshape(score,(y.shape[0],1))
-w=np.reshape(w,(y.shape[0],1))
-mass=np.reshape(mass,(y.shape[0],1))
-pre=np.hstack((y,score,w,mass))
-save_f.create_dataset("pre",data=pre)
+predict=np.hstack((y,score,w))
 
+save_f.create_dataset("predict",data=predict)
+print predict.shape
 
 
 
